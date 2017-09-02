ae4611b7cd24a569d26aa230ed13c79830e08baf
Anil <anil.shafeeque@gmail.com>
2017-09-02 09:06:13 +0300

    Committing Spring boot cassandra first time

---
+++ src/main/java/com/askp/springbootcasandra/SpringBootCasandraApplication.java
@@ -0,0 +1,12 @@
+package com.askp.springbootcasandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCasandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCasandraApplication.class, args);
+	}
+}
---
+++ src/test/java/com/askp/springbootcasandra/SpringBootCasandraApplicationTests.java
@@ -0,0 +1,16 @@
+package com.askp.springbootcasandra;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootCasandraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
