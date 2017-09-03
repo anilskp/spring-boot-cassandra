2fc293f59d17400d5a44e3cab8a2a700892b2596
Anil <anil.shafeeque@gmail.com>
2017-09-03 18:40:42 +0300

    Committing Spring boot cassandra first time f

--- src/main/java/com/askp/springbootcasandra/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.askp.springbootcasandra;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee,String> {
-}
